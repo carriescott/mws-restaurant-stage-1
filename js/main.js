7c6ab390b3a3ad80303fa3a306aa97b84aab564f
carriescott <c.scott@bboxx.co.uk>
2018-04-03 11:39:04 +0100

    feat: Update li elements to enable effective accessibility
    Through the implementation of tabindexes Focus has been added to elements to allow for easy navigation throughout the home page.

--- js/main.js
+++ js/main.js
@@ -137,14 +137,26 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+    li.tabIndex = 0;
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.title = restaurant.name;
+  image.alt = restaurant.photograph_description;
+    image.tabIndex = 0;
   li.append(image);
+
+    // test
+    // const div = document.createElement('div');
+    // div.className = 'test';
+    // const name = document.createElement('h1');
+    // name.innerHTML = restaurant.name;
+    // div.append(name);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+    name.tabIndex = 0;
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -155,23 +167,24 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  // const more = document.createElement('a');
-  // more.innerHTML = 'View Details';
-  // more.href = DBHelper.urlForRestaurant(restaurant);
-  // li.append(more)
+  const more = document.createElement('a');
+  more.innerHTML = 'View Details';
+  more.href = DBHelper.urlForRestaurant(restaurant);
+  li.append(more);
 
     //Create button element in list
-    var btn = document.createElement("BUTTON");
-    var t = document.createTextNode("View Details");
-    btn.appendChild(t);
-    li.append(btn);
-    var myLink = DBHelper.urlForRestaurant(restaurant);
-    console.log(myLink);
-    btn.addEventListener('click', () => {
-        window.location.href = myLink
-    });
+    // var btn = document.createElement("BUTTON");
+    // var t = document.createTextNode("View Details");
+    // btn.appendChild(t);
+    // li.append(btn);
+    // var myLink = DBHelper.urlForRestaurant(restaurant);
+    // console.log(myLink);
+    // btn.addEventListener('click', () => {
+    //     window.location.href = myLink
+    // });
 
   return li
+    // return div
 }
 
 /**
